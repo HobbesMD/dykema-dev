91328be0905fa8a892340a05261110acb4cbfad2
Michael Dykema <mbdykema@gmail.com>
2024-08-19 22:01:50 -0400

    Finish parameters for experience and element changes

--- src/lib/experience.svelte
+++ src/lib/experience.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
 	import SkillList from "./skillList.svelte";
 
+  export let startDate:Date;
+  export let endDate:Date | undefined = undefined;
   export let company:string;
+  export let companyUrl:string;
   export let titles:string[];
   export let responsibilities:string;
   export let skills:string[];
+
+  let startDateString:string, endDateString:string;
+
+  if (endDate == undefined) {
+    startDateString = startDate.getFullYear().toString();
+    endDateString = "Present";
+  } else if (startDate.getFullYear() != endDate.getFullYear()) {
+    startDateString = startDate.getFullYear().toString();
+    endDateString = endDate.getFullYear().toString();
+  } else {
+    startDateString = startDate.toLocaleString('default', {month: 'short'});
+    endDateString = endDate.toLocaleString('default', {month: 'short'}) + endDate.getFullYear().toString();
+  }
 </script>
 
-<div class="grid grid-cols-10 cursor-default max-w-full">
-  <p class="text-slate-400 roboto-mono text-md align-bottom col-span-2">2022 — 2024</p>
-  <div class="ml-4 col-span-8">
-    <a class="roboto-mono font-bold text-slate-100 text-lg hover:" href="https://www.auto-owners.com/" target="_blank">{company} ↗</a>
+<li class="grid grid-cols-9 cursor-default max-w-full mb-8">
+  <p class="text-slate-400 roboto-mono text-md align-bottom col-span-2">{startDateString} — {endDateString}</p>
+  <div class="ml-4 col-span-7">
+    <a class="roboto-mono font-bold text-slate-100 text-lg hover:" href={companyUrl} target="_blank" rel="noreferrer noopener">{company} ↗</a>
     {#each titles as title}
-      <h4 class="roboto-mono font-medium text-slate-300">{title}</h4>
+      <h4 class="roboto-mono font-medium {title == titles[0] ? "text-slate-300" : "text-slate-600"}">{title}</h4>
     {/each}
     <p class="text-slate-400 mt-2">{responsibilities}</p>
     <SkillList skills={skills}/>
   </div>
-</div>
+</li>
