7effb23d3f1d0395b5c4af4a95285c392621cbfe
Michael Dykema <mbdykema@gmail.com>
2024-08-25 16:02:22 -0400

    Add default error page

--- src/lib/experience.svelte
+++ src/lib/experience.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <li class="grid grid-cols-9 cursor-default max-w-full mb-12">
-  <p class="text-slate-400 roboto-mono text-md align-bottom col-span-2">{startDateString} — {endDateString}</p>
+  <p class="text-slate-400 roboto-mono text-md align-bottom col-span-2 pt-1">{startDateString} — {endDateString}</p>
   <div class="ml-4 col-span-7">
     <a class="roboto-mono font-bold text-slate-100 text-lg hover:text-orange" href={companyUrl} target="_blank" rel="noreferrer noopener">{company} ↗</a>
     {#each titles as title}
---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from "$app/stores";
+</script>
+
+<div class="flex flex-col w-full h-screen justify-center items-center roboto-mono">
+  {#if $page.status == 404}
+    <h1 class="text-orange text-9xl">404</h1>
+    <h2 class="text-slate-400 text-5xl mt-8">Where were you trying to go?!</h2>
+  {:else}
+    <h1 class="text-orange text-5xl">You broke something!</h1>
+    <p class="text-slate-400 mt-4 text-2xl">Congratulations?</p>
+  {/if}
+  <a href="/" class="mt-16 text-slate-100 hover:text-orange">Go back home ↩</a>
+</div>
